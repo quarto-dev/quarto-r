e21b4be707b44d4a4bd3924bf5fa3deaa7cb99ab
Salim B <git@salim.space>
2023-10-04 15:41:11 +0200

    Add param `profile` to `quarto_inspect()`

--- R/inspect.R
+++ R/inspect.R
@@ -5,9 +5,14 @@
 #' and dependent resources.
 #'
 #' @param input The input file or project directory to inspect.
+#' @param profile [Quarto project
+#'   profile](https://quarto.org/docs/projects/profiles.html) to use. If
+#'   `NULL`, the default profile is used.
 #'
-#' @return Named list. For input files, the list has members engine, format,
-#'  and resources. For projects the list has members engines and config
+#' @return Named list. For input files, the list contains the elements
+#'   `quarto`, `engines`, `formats`, `resources`, plus `project` if the file is
+#'   part of a Quarto project. For projects, the list contains the elements
+#'   `quarto`, `dir`, `engines`, `config` and `files`.
 #'
 #' @importFrom jsonlite fromJSON
 #'
@@ -18,18 +23,27 @@
 #'
 #' # Inspect project
 #' quarto_inspect("myproject")
-#' }
 #'
+#' # Inspect project's advanced profile
+#' quarto_inspect(
+#'   input = "myproject",
+#'   profile = "advanced"
+#' )}
 #' @export
-quarto_inspect <- function(input = ".") {
+quarto_inspect <- function(input = ".",
+                           profile = NULL) {
 
   quarto_bin <- find_quarto()
 
-  output <- system2(quarto_bin, stdout = TRUE, c(
-    "inspect",
-    path.expand(input)
-  ))
+  output <- system2(
+    command = quarto_bin,
+    args = c(
+      "inspect",
+      if (!is.null(profile)) c("--profile", profile),
+      path.expand(input)
+    ),
+    stdout = TRUE
+  )
 
   fromJSON(output)
 }
-
