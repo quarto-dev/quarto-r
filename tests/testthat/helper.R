182c16850ce6f0169b09e97f847f97acff9eadf3
Christophe Dervieux <christophe.dervieux@gmail.com>
2023-10-12 15:44:37 +0200

    Add local_qmd_file() from rmarkdown helper

--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -5,3 +5,11 @@ skip_if_no_quarto <- function() {
 skip_if_quarto <- function() {
   skip_if(!is.null(quarto_path()))
 }
+
+local_qmd_file <- function(..., .env = parent.frame()) {
+  skip_if_not_installed("xfun")
+  skip_if_not_installed("withr")
+  path <- withr::local_tempfile(.local_envir = .env, fileext = ".qmd")
+  xfun::write_utf8(c(...), path)
+  path
+}
