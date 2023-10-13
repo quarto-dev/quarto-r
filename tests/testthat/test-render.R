1438bd0aa56b69316ccb1aa8f983aa84fbceb4fb
Christophe Dervieux <christophe.dervieux@gmail.com>
2023-10-13 16:18:11 +0200

    Use temp file infrastructure for tests

--- tests/testthat/test-render.R
+++ tests/testthat/test-render.R
@@ -46,19 +46,21 @@ test_that("metadata-file and metadata are merged in quarto_render", {
   )
 })
 
-
 test_that("`quarto_render(as_job = TRUE)` is wrapable", {
-  skip_if(is.null(quarto_path()))
+  skip_on_cran() # this is to test inside RStudio IDE interactively
+  skip_if_no_quarto()
   skip_if_not_installed("rstudioapi")
-  skip_if_not_installed("rprojroot")
   skip_if_not(rstudioapi::isAvailable())
-  dir <- rprojroot::find_testthat_root_file()
-  input <- file.path(dir, "test.Rmd")
-  output <- file.path(dir, "test.html")
-  wrapper <- function(path) quarto_render(path, quiet = TRUE, as_job = TRUE)
-  wrapper(input)
+  qmd <- local_qmd_file(c("content"))
+  withr::local_dir(dirname(qmd))
+  output <- basename(
+    withr::local_file(xfun::with_ext(qmd, ".native"))
+  )
+  wrapper <- function(path, out, format) {
+    quarto_render(path, output_file = out, output_format = format, quiet = TRUE, as_job = TRUE)
+  }
+  wrapper(basename(qmd), output, "native")
   # wait for background job to finish (10s should be conservative enough)
   Sys.sleep(10)
   expect_true(file.exists(output))
-  unlink(output)
 })
