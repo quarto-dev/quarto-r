8a083a481a67f13808bd8ee8f2302692c45b7963
Christophe Dervieux <christophe.dervieux@gmail.com>
2024-01-31 18:58:42 +0100

    Avoid double printing error message
    quarto does put everything in stderr so when error we were printing two times.
    
    Let's catch first, and then print non error output if any

--- R/quarto.R
+++ R/quarto.R
@@ -49,7 +49,7 @@ quarto_version <- function() {
 #' @importFrom processx run
 quarto_run <- function(args = character(), quarto_bin = find_quarto(), echo = FALSE, echo_cmd = getOption("quarto.echo_cmd", FALSE), ..., .call = rlang::caller_env()) {
   res <- tryCatch({
-    processx::run(quarto_bin, args = args, echo = echo, error_on_status = TRUE, echo_cmd = echo_cmd, ...)
+    processx::run(quarto_bin, args = args, echo = FALSE, error_on_status = TRUE, echo_cmd = echo_cmd, ...)
   },
     error = function(e) {
       msg <- c("Error running quarto cli:")
@@ -58,6 +58,11 @@ quarto_run <- function(args = character(), quarto_bin = find_quarto(), echo = FA
       rlang::abort(msg, call = .call, parent = e)
     }
   )
+  # quarto outputs any message into stderr as stdout is reserved
+  # for regular rendered output (that we don't support in this package yet)
+  if (echo && nzchar(res$stderr)) {
+    cli::cat_line(res$stderr)
+  }
   invisible(res)
 }
 
