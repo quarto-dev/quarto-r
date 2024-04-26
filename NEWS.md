9275b2dfc272f7c6d9c41ce67e90b77cb2bc7e8c
Christophe Dervieux <christophe.dervieux@gmail.com>
2024-04-26 12:16:28 -0400

    Fix check_extension_approval usage
    fixes #172

--- NEWS.md
+++ NEWS.md
@@ -1,5 +1,7 @@
 # quarto (development version)
 
+- Approval check in `quarto_add_extension()` and `quarto_use_template()` now works correctly (thanks, @eveyp, #172).
+
 # quarto 1.4
 
 - This version is now adapted to Quarto 1.4 latest stable release.
--- R/utils-prompt.R
+++ R/utils-prompt.R
@@ -1,23 +1,23 @@
 check_extension_approval <- function(no_prompt = FALSE, what = "Something", see_more_at = NULL) {
-  if (!no_prompt) {
-    if (!rlang::is_interactive()) {
-      cli::cli_abort(c(
-        "{ what } requires explicit approval.",
-        ">" = "Set {.arg no_prompt = TRUE} if you agree.",
-        if (!is.null(see_more_at)) {
-          c(i = "See more at {.url {see_more_at}}")
-        }
-      ))
-    } else {
-      cli::cli_inform(c(
-        "{what} may execute code when documents are rendered. ",
-        "*" = "If you do not trust the author(s) of this {what}, we recommend that you do not install or use this {what}."
-      ))
-      prompt_value <- tolower(readline(sprintf("Do you trust the authors of this %s (Y/n)? ", what)))
-      if (!prompt_value %in% "y") {
-        cli::cli_inform("{what} not installed.")
-        return(invisible(FALSE))
+  if (no_prompt) return(TRUE)
+
+  if (!rlang::is_interactive()) {
+    cli::cli_abort(c(
+      "{ what } requires explicit approval.",
+      ">" = "Set {.arg no_prompt = TRUE} if you agree.",
+      if (!is.null(see_more_at)) {
+        c(i = "See more at {.url {see_more_at}}")
       }
+    ))
+  } else {
+    cli::cli_inform(c(
+      "{what} may execute code when documents are rendered. ",
+      "*" = "If you do not trust the author(s) of this {what}, we recommend that you do not install or use this {what}."
+    ))
+    prompt_value <- tolower(readline(sprintf("Do you trust the authors of this %s (Y/n)? ", what)))
+    if (!prompt_value %in% "y") {
+      cli::cli_inform("{what} not installed.")
+      return(invisible(FALSE))
     }
   }
 }
