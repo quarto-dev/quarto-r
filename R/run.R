d9fc60fdf5899c5bdc48ed58794b9a1fc20c1af0
Salim B <git@salim.space>
2024-07-15 13:44:58 +0200

    Fix typos (#197)
    Co-authored-by: Christophe Dervieux <christophe.dervieux@gmail.com>

--- R/run.R
+++ R/run.R
@@ -2,9 +2,9 @@
 #'
 #' Serve a Shiny interactive document. By default, the document will
 #' be rendered first and then served If you have previously rendered
-#' the document, pass `render - FALSE` to skip rendering.
+#' the document, pass `render = FALSE` to skip rendering.
 #'
-#' @param input The input file to run Should be a file with
+#' @param input The input file to run. Should be a file with
 #'   a `server: shiny` entry in its YAML front-matter.
 #' @param render Render the document before serving it.
 #'
