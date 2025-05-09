5b2c26f73fe211a4526530dba96930664874c64f
Christophe Dervieux <christophe.dervieux@gmail.com>
2025-05-09 15:34:03 +0200

    rewrites test of prompts

--- R/utils-prompt.R
+++ R/utils-prompt.R
@@ -1,29 +1,34 @@
 check_approval <- function(
   no_prompt = FALSE,
   what = "Something",
+  not_action = "approved",
   see_more_at = NULL,
   prompt_message = NULL,
-  interactive_info = NULL # could use `{ what }` as used in `cli_inform()`
+  interactive_info = NULL, # could use `{ what }` as used in `cli_inform()`
+  .call = rlang::caller_env()
 ) {
   if (no_prompt) return(TRUE)
 
   if (!is_interactive()) {
-    cli::cli_abort(c(
-      "{ what } requires explicit approval.",
-      ">" = "Set {.arg no_prompt = TRUE} if you agree.",
-      if (!is.null(see_more_at)) {
-        c(i = "See more at {.url {see_more_at}}")
-      }
-    ))
+    cli::cli_abort(
+      c(
+        "{ what } requires explicit approval.",
+        ">" = "Set {.arg no_prompt = TRUE} if you agree.",
+        if (!is.null(see_more_at)) {
+          c(i = "See more at {.url {see_more_at}}")
+        }
+      ),
+      call = .call
+    )
   } else {
     if (!is.null(interactive_info)) {
       cli::cli_inform(interactive_info)
     }
     prompt_value <- tolower(readline(prompt_message))
     if (!prompt_value %in% c("", "y")) {
-      cli::cli_inform(paste0(what, " not installed."))
+      cli::cli_alert_info(paste0(what, " not {not_action}"))
+      return(invisible(FALSE))
     }
-    return(invisible(FALSE))
   }
   return(invisible(TRUE))
 }
@@ -46,6 +51,7 @@ check_extension_approval <- function(
   check_approval(
     no_prompt = no_prompt,
     what = what,
+    not_action = "installed",
     see_more_at = see_more_at,
     prompt_message = prompt_message,
     interactive_info = interactive_info
@@ -65,6 +71,7 @@ check_removal_approval <- function(
   check_approval(
     no_prompt = no_prompt,
     what = what,
+    not_action = "removed",
     see_more_at = see_more_at,
     prompt_message = prompt_message,
     interactive_info = NULL
--- tests/testthat/test-utils-prompt.R
+++ tests/testthat/test-utils-prompt.R
@@ -1,60 +1,101 @@
-test_that("Checking extension with approval prompt mocked y", {
+test_that("Checking non interactive approval", {
+  rlang::local_interactive(FALSE)
+  expect_snapshot(expect_true(check_approval(TRUE, "My thing")))
+  expect_snapshot(error = TRUE, {
+    check_approval(FALSE, "My thing")
+  })
+  expect_snapshot(error = TRUE, {
+    check_approval(FALSE, "My thing", see_more_at = "https://example.com")
+  })
+})
+
+
+test_that("Checking interactive approval with prompt mocked y", {
   local_mocked_bindings(
-    readline = function(...) "y",
-    is_interactive = function() TRUE
+    readline = function(...) "y"
   )
+  rlang::local_interactive(TRUE)
   expect_true({
-    check_extension_approval(
-      FALSE,
-      "Quarto extensions",
-      "https://quarto.org/docs/extensions/managing.html"
-    )
+    check_approval(FALSE, "my-thing")
   })
 })
 
-test_that("Checking extension with approval prompt mocked n", {
+test_that("Checking interactive approval with prompt mocked n", {
   local_mocked_bindings(
-    readline = function(...) "n",
-    is_interactive = function() TRUE
+    readline = function(...) "n"
   )
-  expect_false({
+  rlang::local_interactive(TRUE)
+  expect_snapshot(expect_false({
+    check_approval(FALSE, "my-thing", see_more_at = "https://example.com")
+  }))
+})
+
+test_that("Checking non interactive extension approval", {
+  rlang::local_interactive(FALSE)
+  expect_snapshot(expect_true(check_extension_approval(TRUE, "My thing")))
+  expect_snapshot(error = TRUE, {
+    check_extension_approval(FALSE, "My thing")
+  })
+  expect_snapshot(error = TRUE, {
     check_extension_approval(
       FALSE,
-      "Quarto extensions",
-      "https://quarto.org/docs/extensions/managing.html"
+      "My thing",
+      see_more_at = "https://example.com"
     )
   })
 })
 
-test_that("Checking extension approval", {
-  skip_if_no_quarto()
-  skip_if_offline("github.com")
+test_that("Checking interactive extension approval with prompt mocked y", {
+  local_mocked_bindings(
+    readline = function(...) "y"
+  )
+  rlang::local_interactive(TRUE)
+  expect_snapshot(expect_true({
+    check_extension_approval(FALSE, "my-thing")
+  }))
+})
 
-  expect_true(check_extension_approval(
-    TRUE,
-    "Quarto extensions",
-    "https://quarto.org/docs/extensions/managing.html"
-  ))
-  expect_true(check_extension_approval(
-    TRUE,
-    "Quarto templates",
-    "https://quarto.org/docs/extensions/formats.html#distributing-formats"
-  ))
+test_that("Checking interactive extension approval with prompt mocked n", {
+  local_mocked_bindings(
+    readline = function(...) "n"
+  )
+  rlang::local_interactive(TRUE)
+  expect_snapshot(expect_false({
+    check_extension_approval(FALSE, "my-thing")
+  }))
+})
 
-  expect_error({
-    local_reproducible_output(rlang_interactive = FALSE)
-    check_extension_approval(
+test_that("Checking non interactive removal approval", {
+  rlang::local_interactive(FALSE)
+  expect_snapshot(expect_true(check_removal_approval(TRUE, "My thing")))
+  expect_snapshot(error = TRUE, {
+    check_removal_approval(FALSE, "My thing")
+  })
+  expect_snapshot(error = TRUE, {
+    check_removal_approval(
       FALSE,
-      "Quarto extensions",
-      "https://quarto.org/docs/extensions/managing.html"
+      "My thing",
+      see_more_at = "https://example.com"
     )
   })
-  expect_error({
-    local_reproducible_output(rlang_interactive = FALSE)
-    check_extension_approval(
-      TRUE,
-      "Quarto extensions",
-      "https://quarto.org/docs/extensions/managing.html"
-    )
-  })
+})
+
+test_that("Checking interactive removal approval with prompt mocked y", {
+  local_mocked_bindings(
+    readline = function(...) "y"
+  )
+  rlang::local_interactive(TRUE)
+  expect_snapshot(expect_true({
+    check_removal_approval(FALSE, "my-thing")
+  }))
+})
+
+test_that("Checking interactive removal approval with prompt mocked n", {
+  local_mocked_bindings(
+    readline = function(...) "n"
+  )
+  rlang::local_interactive(TRUE)
+  expect_snapshot(expect_false({
+    check_removal_approval(FALSE, "my-thing")
+  }))
 })
