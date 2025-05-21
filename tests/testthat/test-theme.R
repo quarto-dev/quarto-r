8b2a3ba5ad93bf4cf9f0d4a5d512b13c098e0ebc
Gordon Woodhull <gordon.woodhull@posit.co>
2025-05-21 05:42:34 -0400

    test themed packages conditionally (#243)

--- tests/testthat/test-theme.R
+++ tests/testthat/test-theme.R
@@ -7,25 +7,37 @@ skip_if_not_installed("withr")
 install_dev_package()
 
 test_that("render flextable", {
+  skip_if_not_installed("bslib")
+  skip_if_not_installed("flextable")
   .render(test_path("theme/flextable.qmd"))
 })
 
 test_that("render ggiraph", {
+  skip_if_not_installed("bslib")
+  skip_if_not_installed("ggiraph")
   .render(test_path("theme/ggiraph.qmd"))
 })
 
 test_that("render ggplot2", {
+  skip_if_not_installed("bslib")
+  skip_if_not_installed("ggplot2")
   .render(test_path("theme/ggplot2.qmd"))
 })
 
 test_that("render gt", {
+  skip_if_not_installed("bslib")
+  skip_if_not_installed("gt")
   .render(test_path("theme/gt.qmd"))
 })
 
 test_that("render plotly-r", {
+  skip_if_not_installed("bslib")
+  skip_if_not_installed("plotly")
   .render(test_path("theme/plotly-r.qmd"))
 })
 
 test_that("render thematic", {
+  skip_if_not_installed("bslib")
+  skip_if_not_installed("thematic")
   .render(test_path("theme/thematic.qmd"))
 })
