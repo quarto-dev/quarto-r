1eadf3164f658983375251d31df121deaa7a04df
Christophe Dervieux <christophe.dervieux@gmail.com>
2025-07-01 18:12:49 +0200

    fix the usage of internal storage env

--- R/aaa.R
+++ R/aaa.R
@@ -2,9 +2,11 @@
 # Using convention in https://github.com/tidyverse/design/issues/126
 the <- rlang::new_environment(
   list(
-    preview_infos = list(),
-    latest_stable = list(date = NULL, infos = NULL),
-    latest_prerelease = list(date = NULL, infos = NULL)
+    preview_infos = list()
+
+    # other possibles values in this env but should not exist because of env_cache usage
+    # latest_stable = list(date = NULL, infos = NULL),
+    # latest_prerelease = list(date = NULL, infos = NULL)
   ),
   parent = emptyenv()
 )
