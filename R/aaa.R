25e014fb9ab201a9be962860ec54a9104cfff82e
Christophe Dervieux <christophe.dervieux@gmail.com>
2025-07-01 16:37:34 +0200

    fixup the internal state environment definition

--- R/aaa.R
+++ R/aaa.R
@@ -1,6 +1,6 @@
-#' Internal package state
-#' @noRd
-the <- new.env(
+# internal environment acting as storage and cache
+# Using convention in https://github.com/tidyverse/design/issues/126
+the <- rlang::new_environment(
   list(
     latest_stable = list(date = NULL, infos = NULL),
     latest_prerelease = list(date = NULL, infos = NULL)
