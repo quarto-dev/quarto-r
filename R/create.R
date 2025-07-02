70a24ff5fd09f9ce8a76432b4247e01533afaef1
Christophe Dervieux <christophe.dervieux@gmail.com>
2025-07-02 13:18:16 +0200

    Support title in project creation
    closes  #148 - Behavior of `quarto_create_project()` differs from `quarto create-project` since the former "requires" the creation of a new directory

--- R/create.R
+++ R/create.R
@@ -11,7 +11,11 @@
 #'
 #' @param type The type of project to create. As of Quarto 1.4, it can be one of
 #'   `r paste0("\\code{", paste(quarto_project_type, collapse = "}, \\code{"),"}")`.
-#' @param name The name of the project and the directory that will be created.
+#' @param name The name of the project and the directory that will be created. Special case
+#' is to use `name = "."` to create the project in the current directory. In that case provide `title`
+#' to set the project title.
+#' @param title The title of the project. By default, it will be the name of the project, same as directory name created.
+#' or "My project" if `name = "."`. If you want to set a different title, provide it here.
 #' @param dir The directory in which to create the new Quarto project, i.e. the
 #'   parent directory.
 #'
@@ -23,7 +27,17 @@
 #'
 #' @examples
 #' \dontrun{
+#' # Create a new project directory in another directory
 #' quarto_create_project("my-first-quarto-project", dir = "~/tmp")
+#'
+#' # Create a new project directory in the current directory
+#' quarto_create_project("my-first-quarto-project")
+#'
+#' # Create a new project with a different title
+#' quarto_create_project("my-first-quarto-project", title = "My Quarto Project")
+#'
+#' # Create a new project inside the current directory directly
+#' quarto_create_project(".", title = "My Quarto Project")
 #' }
 #'
 #'
@@ -32,6 +46,7 @@ quarto_create_project <- function(
   name,
   type = "default",
   dir = ".",
+  title = name,
   no_prompt = FALSE,
   quiet = FALSE,
   quarto_args = NULL
@@ -47,8 +62,16 @@ quarto_create_project <- function(
   }
 
   if (rlang::is_interactive() && !no_prompt) {
+    folder_msg <- if (name != ".") {
+      "as a folder named {.strong {name}}"
+    }
     cli::cli_inform(c(
-      "This will create a new Quarto {.emph {type}} project as a folder named {.strong {name}} in {.path {xfun::normalize_path(dir)}}."
+      paste(
+        "This will create a new Quarto {.emph {type}} project",
+        folder_msg,
+        "in {.path {xfun::normalize_path(dir)}}."
+      ),
+      "Project title will be set to {.strong {title}}."
     ))
     prompt_value <- tolower(readline(sprintf("Do you want to proceed (Y/n)? ")))
     if (!prompt_value %in% c("", "y")) {
@@ -62,7 +85,7 @@ quarto_create_project <- function(
     "project",
     type,
     name,
-    name,
+    title,
     "--no-prompt",
     "--no-open",
     if (is_quiet(quiet)) cli_arg_quiet(),
