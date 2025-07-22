36500949078ef5d732dd09b58dc27cff05aac829
Christophe Dervieux <christophe.dervieux@gmail.com>
2025-07-22 15:47:14 +0200

    use yaml::verbatim_logical directly

--- R/utils.R
+++ R/utils.R
@@ -6,12 +6,7 @@ relative_to_wd <- function(path) {
 # Specific YAML handlers
 # as quarto expects YAML 1.2 and yaml R package supports 1.1
 yaml_handlers <- list(
-  # Handle yes/no from 1.1 to 1.2
-  # https://github.com/vubiostat/r-yaml/issues/131
-  logical = function(x) {
-    value <- ifelse(x, "true", "false")
-    structure(value, class = "verbatim")
-  }
+  logical = yaml::verbatim_logical
 )
 
 #' @importFrom yaml as.yaml
